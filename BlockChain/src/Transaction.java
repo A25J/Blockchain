4a974e30d11abf9a4b13d8f19fc835ad254c90b0
rubahassan1 <22130312@students.liu.edu.lb>
2026-03-25 19:41:07 +0200

    --

---
+++ BlockChain/src/Transaction.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ruba Hassan
+ */
+public class Transaction {
+    String fromWallet;
+    String toWallet;
+    int amount;
+    
+    public Transaction(String fromWallet, String toWallet,int amount){
+        this.fromWallet=fromWallet;
+        this.toWallet=toWallet;
+        this.amount=amount;
+    }
+}
